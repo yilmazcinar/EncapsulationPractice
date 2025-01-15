a31b770167392a3ad9272bbd5246d5b2d6e78cdb
yilmazcinar <cinaryilmaz96@gmail.com>
2025-01-15 23:13:25 +0300

    Add files via upload

---
+++ EncapsulationOOPProject/EncapsulationOOPProject/Car.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulationOOPProject
+{
+    internal class Car
+    {
+        public string Marka { get; set; }
+
+        public int Model {  get; set; } 
+       
+        public string Renk { get; set; }
+
+        int _kapiSayisi;
+
+        public int KapıSayisi {
+
+            get
+            {
+                return _kapiSayisi;
+
+
+            }
+
+            set
+            {
+                if (value != 4 && value != 2)
+                {
+                    Console.WriteLine("\nKapı sayısı 4 ve 2 den farklı olamaz !\n");
+
+                    _kapiSayisi = -1;
+                }
+
+               else
+                {
+                    _kapiSayisi = value;
+                }
+            } 
+        
+        
+        
+        }
+
+
+
+
+    }
+}
---
+++ EncapsulationOOPProject/EncapsulationOOPProject/Program.cs
@@ -0,0 +1,33 @@
+﻿using EncapsulationOOPProject;
+
+Car newCar1 = new Car()
+{
+    Marka = "Tofaş",
+    Model = 1992,
+    Renk = "Sarı",
+    KapıSayisi = 4
+
+
+};
+
+
+
+
+
+Console.WriteLine($"Arabanın Markası : {newCar1.Marka}\n\nModeli : {newCar1.Model}\n\nRengi : {newCar1.Renk}\n\nKapı Sayısı : {newCar1.KapıSayisi}");
+
+Console.WriteLine("--------------------------------------------------------------");
+
+Car newCar2 = new Car()
+{
+    Marka = "Ferrari",
+    Model = 2024,
+    Renk = "Kırmızı",
+    KapıSayisi = 7 
+
+
+};
+
+
+
+Console.WriteLine($"Arabanın Markası : {newCar2.Marka}\n\nModeli : {newCar2.Model}\n\nRengi : {newCar2.Renk}\n\nKapı Sayısı : {newCar2.KapıSayisi}");
